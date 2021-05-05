ba1f8d0a901ee08d4a546ef042dda4d01a0ad18d
moutaz-hegazy <moutazhegazy93@gmail.com>
2021-05-05 15:02:30 +0200

    Initial Commit

---
+++ MVVM_lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_lab
+//
+//  Created by moutaz hegazy on 5/5/21.
+//  Copyright © 2021 moutaz_hegazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
